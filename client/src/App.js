df0dc61c1671a2241479dd6fdb160899a87f4fd7
Raymond _ <raymondt1234@gmail.com>
2020-01-23 13:56:07 -0700

    stuff broken commit is only to update the repo

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,18 @@ import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 import NavBar from "./components/NavBar";
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <NavBar/>
-          <Switch>
-            <Route exact path="/" component={Search}></Route>
-            <Route exact path="/search" component={Search}></Route>
-            <Route path="/saved" component={Saved}></Route>
-            <Route component={NoMatch}></Route>
-          </Switch>
+        <Switch>
+          <Route exact path="/" component={Search}></Route>
+          <Route exact path="/search" component={Search}></Route>
+          <Route path="/saved" component={Saved}></Route>
+          <Route component={NoMatch}></Route>
+        </Switch>
       </Router>
     );
   }

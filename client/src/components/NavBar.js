541c3527e34faa1d47c010915afefeb720741e21
raymondt1234 <raymondt1234@gmail.com>
2020-01-23 17:31:11 -0700

    Page routing works

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,6 +3,7 @@ import React from "react";
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <h1>Book </h1>
       <a className="navbar-brand" href="/">
         Search
       </a>
